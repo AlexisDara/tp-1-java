5a3c0729ff3bce7076b7ee1a7c28c6f3c9b9fd85
AlexisDara <daraioalexis@gmail.com>
2025-06-06 16:41:32 -0300

    Create ej9.java

---
+++ ej9.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class ej4{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    
+        int[] n = new int[3];
+            System.out.println("Ingrese 3 numeros");     
+     for(int i = 0; i < n.length; i++) {
+            n[i] = scanner.nextInt();
+        }
+        int mayor=0;
+        for(int j = 0;j<n.length;j++){
+            if(n[j]>mayor){
+                mayor=n[j];
+            }
+            
+        }
+       
+        System.out.println("El mayor de los tres es: " + mayor);
+        
+    }
+    
+}
