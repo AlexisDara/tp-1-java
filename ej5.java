0da318785ab94b5b47bd88a29cf5c85a516a9c45
AlexisDara <daraioalexis@gmail.com>
2025-06-06 15:48:08 -0300

    Create ej5.java

--- ej5.java
+++ ej5.java
@@ -7,9 +7,12 @@ public class ej5{
         int n = 0;
         int aux = 0;
         
-        System.out.println("Ingrese un numero:");
+        System.out.println("Ingrese un numero del 1 al 10:");
         n = scanner.nextInt();
-
+        while(n<= 0 || n>=11){
+            System.out.println("ingrese un numero del 1 al 10:");
+            n = scanner.nextInt();
+        }
         // recorrer desde 1 hasta 10
         for(int i = 1; i <= 10; i++) {
            aux=0;
