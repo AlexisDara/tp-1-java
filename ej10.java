69aea84de574c5be79a5c2a90cf74dfda4b1614f
AlexisDara <daraioalexis@gmail.com>
2025-06-06 16:51:33 -0300

    Create ej10.java

---
+++ ej10.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class ej4{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    
+        int n = 0;
+          
+        System.out.println("ingrese un numero");
+        n=scanner.nextInt();
+       
+        int aux= 1;     
+      
+        for(int i =1;i<=n;i++){      
+        aux *=i;
+        }
+
+        System.out.println("el resultado es: " + aux);
+        
+    }
+    
+}
