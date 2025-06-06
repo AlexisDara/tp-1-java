b08c9ff0a61841735fb36e4a868ac0ff318e200c
AlexisDara <daraioalexis@gmail.com>
2025-06-06 16:00:11 -0300

    Create ej6.java

---
+++ ej6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ej4{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.println("ingrese una palabra");
+        String n= scanner.nextLine();
+        
+        String nInvertido= "";
+        
+        
+        for(int i = n.length() -1 ; i >= 0; i--) {
+           nInvertido += n.charAt(i);
+       
+        }
+    
+         System.out.println(" palabra normal:" + n);// mostrar palabra ordenada
+         System.out.println(" palabra invertida:" + nInvertido);// mostrar palabra invertida
+
+    }
+}
