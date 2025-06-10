06094ad1fe09ebac035f50bade9d336329987e53
AlexisDara <daraioalexis@gmail.com>
2025-06-10 15:59:53 -0300

    Update ej14.java

--- ej14.java
+++ ej14.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+public class ej14{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("ingrese un numero N: ");
+        int n = scanner.nextInt();
+        
+        int primero = 0;
+        int segundo = 1;
+        
+        System.out.print("secuencia de fibonacci: ");
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print(primero + " ");
+            
+            int siguiente = primero + segundo;
+            primero = segundo;
+            segundo = siguiente;
+        }
+        
+        scanner.close();
+    }
+}
